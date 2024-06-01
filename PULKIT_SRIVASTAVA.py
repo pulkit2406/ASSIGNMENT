7e2738f1658ca47ad2b68620068026aa920289ee
Pulkit Srivastava <pulkitald@gmail.com>
2024-06-01 23:14:47 +0530

    Add files via upload

---
+++ PULKIT_SRIVASTAVA.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+(3*4)+5
+# This a simple arithmetic expression to mutiply then add integers
+
+
+# In[4]:
+
+
+minute = 200
+hour = minute/60
+# This will convert 200 minutes to hours by diving by 60
+hour
+
